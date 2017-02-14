f1ad6ce08ac910982c5d35290a62ce9f6b6f2fef
Kelly Davis <kellydavis1974@gmail.com>
2017-02-13 21:22:08 -0500

    created theme picker landing page - user picks between sports or bands theme

--- assets/hangman.js
+++ assets/hangman.js
@@ -1,42 +1,39 @@
-/*hangman pseudo coding
-
-idea: themed elements in object for switching
-var theme = <userpick>
-
-themes = {
-sports: 
-{
-football:{image:'football.jpg'},
-baseball:{image:'baseball.jpg'},
-cricket:{image:'cricket.jpg'},
-soccer:{image:'soccer.jpg'},
-basketball:{image:'basketball.jpg'},
-hunting:{image:'hunting.jpg'},
-racing:{image:'racing.jpg'},
-softball:{image:'softball.jpg'},
-volleyball:{image:'volleyball.jpg'},
-swimming:{image:'swimming.jpg'}
-},	
+var gameTracker = {
 	
-bands: 
-{
-'the beatles':{image:'beatles.jpg'}
-'metallica':{image:'metallica.jpg'},
-'the who':{image:'who.jpg'},
-'radiohead':{image:'radiohead.jpg'},
-'the smiths':{image:'smiths.jpg'},
-'the velvet underground':{image:'velvet.jpg'},
-'led zeppelin':{image:'zeppelin.jpg'},
-'the rolling stones':{image:'stones.jpg'},
-'nirvana':{image:'nirvana.jpg'},
-'wheezer':{image:'wheezer.jpg'}
-} 
+}
+var themes = {
+	sports: 
+	{	
+		football:{image:'football.jpg'},
+		baseball:{image:'baseball.jpg'},
+		cricket:{image:'cricket.jpg'},
+		soccer:{image:'soccer.jpg'},
+		basketball:{image:'basketball.jpg'},
+		hunting:{image:'hunting.jpg'},
+		racing:{image:'racing.jpg'},
+		softball:{image:'softball.jpg'},
+		volleyball:{image:'volleyball.jpg'},
+		swimming:{image:'swimming.jpg'}
+	},	
+		bands: 
+	{
+		'the beatles':{image:'beatles.jpg'},
+		'metallica':{image:'metallica.jpg'},
+		'the who':{image:'who.jpg'},
+		'radiohead':{image:'radiohead.jpg'},
+		'the smiths':{image:'smiths.jpg'},
+		'the velvet underground':{image:'velvet.jpg'},
+		'led zeppelin':{image:'zeppelin.jpg'},
+		'the rolling stones':{image:'stones.jpg'},
+		'nirvana':{image:'nirvana.jpg'},
+		'wheezer':{image:'wheezer.jpg'}
+	} 
 }
 
-gameTracker.theme = theme
+//gameTracker.theme = theme
 
 //use theme switcher to initiate game
-
+/*
 gameTracker.word = <random word chosen from theme's bank>
 
 Wins: show wins
@@ -64,3 +61,6 @@ letters: 0
 }
 
 */
+function initialize(){
+
+}
