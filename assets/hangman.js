a02854adde3e6165b4225f9c3b441cd9fcf60f59
Kelly Davis <kellydavis1974@gmail.com>
2017-02-17 23:11:22 -0500

    updated some initialization steps to simplify; added checkKey function to test key presses

--- assets/hangman.js
+++ assets/hangman.js
@@ -1,5 +1,6 @@
-var gameTracker = {};
-
+var gameTracker = {
+	guesses:15
+};
 var themes = {
 	sports: 
 		{
@@ -37,67 +38,35 @@ var themes = {
 		} 
 }
 
-//gameTracker.theme = theme
-
-//use theme switcher to initiate game
-/*
-gameTracker.word = <random word chosen from theme's bank>
-
-Wins: show wins
-
-display current word
-
-
-guess counter
-display guesses in this round
-(start at 15)
-
-
-letters tracker:
-display letters guessed in this round
-
-
-
-
-gameTracker = {
-
-wins: 0,
-word: <current word>, 
-guessesRemaining: 15,
-letters: 0
-}
-
-word guess engine:
-
-use object that uses word characters as keys and initial values as "_":
-var wordChars = {};
-for (var i=0;i<gameTracker.word.length;i++)
-	wordChars[gameTracker.word[i]] = '_';
-
-
-*/
 function initialize(theme) {
+	// set onkeyup listener; is key released? go to checkKey function
+	$(document).on("keyup",function(){checkKey(event)});
+
 	// hide themepicker
-	picker=document.getElementById('themepicker');
-	picker.style.display="none";
+	picker=$('#themepicker');
+	$(picker).fadeOut("fast");
+	
+	// set guesses remaining
+	guessSpan = $("#remaining");
+	$(guessSpan).html(gameTracker.guesses); 
+
 	// from user choice, set theme in gameTracker
 	gameTracker.theme = themes[theme];
+	
 	// from user choice, set theme background
 	document.getElementsByTagName('body')[0].style.backgroundImage = 'url(assets/images/'+themes[theme].background;
-	// how many words in this theme's bank?
-	// put words in an array
-	bank=[];
-	for (var key in gameTracker.theme.bank) {
-		bank.push(key);
-	}
-	// choose a random word
+	
+	// we need to pick a random word from the theme's bank
+	// put words in an array using Object.keys function
+	bank=Object.keys(gameTracker.theme.bank);
+		// choose a random word
 	gameTracker.word=bank[randNum(bank.length)];
 	
-	// now we also have an image
+	// get associated image
 	gameTracker.image=gameTracker.theme.bank[gameTracker.word].image;
-
 	$("#gameImage").append('<img id="imageHint">');
-	$("#imageHint").attr("src","assets/images/"+gameTracker.image);	
+	var imageHint = $("#imageHint");
+	imageHint.attr("src","assets/images/"+gameTracker.image);	
 	
 	
 	// update stage header
@@ -109,29 +78,91 @@ function initialize(theme) {
 	$("#gameStage").fadeIn(2000);
 
 // create array of wordChars with word chars as keys, boolean false (0) as initial values
-	var letters= [];
-	for (var i=0;i<gameTracker.word.length;i++) {
-		letters[i] = {[gameTracker.word[i]]:0};  // assign gameTracker.word letters as [literals] for keys
-		if (gameTracker.word[i]==' ')  // but if there's a space in the word, make the value 1
-			letters[i] = {[gameTracker.word[i]]:1} 
+	var word=gameTracker.word.split('');
+	for (var i=0;i<word.length;i++) {
+		
+		// initial state is false for guess value
+		var thisValue = 0;
+		
+		// except we'll give them spaces as guessed
+		if (word[i] == ' ')
+			thisValue= 1;
+
+		// modify array so each letter becomes an object with a boolean value; use [literal] assignment for keys
+		word[i] = {[word[i]]:thisValue};   
 	}
-	gameTracker.letters = letters;
+
+	// update gameTracker object with letters, then run updateWord function
+	gameTracker.letters = word;
 	updateWord();
 
 }
+
+
 function updateWord() {
 	var wordDiv = document.getElementById('word');
-	wordDiv.innerHTML = '';
-	for (var i=0;i<gameTracker.letters.length;i++) {
+	wordDiv.innerHTML = '';  // start with clear div each time
+	var correct=0;
+	// loop through gameTracker.letters array
+	for (var i=0;i<gameTracker.letters.length;i++) {  
+		
+		// retrieve actual letter of word
 		var thisLetter = Object.keys(gameTracker.letters[i])[0];
-		if (gameTracker.letters[i].thisLetter)
-			wordDiv.innerHTML+=thisLetter
+		
+		// value of the gameTracker.letters[i] object is boolean: true if user guessed (or was a space), false if not yet guessed 
+		if (gameTracker.letters[i].thisLetter) { // true
+			wordDiv.innerHTML+=thisLetter;
+			correct++				// track number of correct letters
+			}    
 		else 
-			wordDiv.innerHTML += '_';
+			wordDiv.innerHTML += ' _ ';  // false - show underscore
 	}
+
+	// update image hint; height of image div is percent of correct answers
+	// #imageHint is the actual img; #gameImage is the wrapper, initially set to height=0; 
+	// this only works because all the images have the same height/width ratio of 1.5
+	var totalHeight = $(imageHint).prop( "offsetWidth")/1.5;
+	var newHeight = totalHeight*(correct/gameTracker.letters.length);
+	console.log('% correct: '+correct/gameTracker.letters.length);
+	$("#gameImage").css("height",newHeight);
+
+}
+
+function checkKey() {
+ 	var userGuess = event.key;
+ 	console.log('key: '+userGuess);
+ 	
+ 	// only counts as a guess if is letter
+ 	if (userGuess.match(/[a-z]/i)) {
+ 		
+ 		// decrement gameTracker.guesses
+ 		(gameTracker.guesses)--;
+ 		$(guessSpan).html(gameTracker.guesses); 
+ 		// out of guesses? game over, man!
+ 		if (gameTracker.guesses < 0)
+ 			gameOver();
+
+ 		// add letter to "guessed letters" list
+ 		$("#guessed").append('<span class="letter">'+userGuess+'</span>');
+
+ 		// is this one of our word's letters?
+ 		for (var i=0;i<gameTracker.letters.length;i++) {
+ 			if (userGuess == Object.keys(gameTracker.letters[i])[0]) {
+ 				gameTracker.letters[i] = 1;
+ 				updateWord();
+ 			}
+ 		}
+
+ 	}  // end is-letter code
+ 	
+}  // end checkKey() function
+
+function gameOver(){
+	// create pop-up window that ends game 
+	alert('game over');
 }
 
 function randNum(max) {
-   	var num = Math.floor((Math.random()*max)+1);
+   	var num = Math.floor((Math.random()*max))
    	return num;
  }
